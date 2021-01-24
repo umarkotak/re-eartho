153765944f96a7a4d0f28e2b3615445f852d1e6d
umar.ramadhana <umar.ramadhana@gmail.com>
2021-01-24 16:26:08 +0700

    create implementation of like and unlike

--- app/controllers/global/content_likes_controller.rb
+++ app/controllers/global/content_likes_controller.rb
@@ -2,10 +2,38 @@ module Global
   class ContentLikesController < ApiController
     def like
       authenticate_user
+
+      content_like = ContentLike.find_by(content_id: content.id, user_id: @user.id)
+      raise "user already like this content" if content_like.present?
+
+      content.transaction do
+        content.update!(count_like: content.count_like + 1)
+        ContentLike.create!(content_id: content.id, user_id: @user.id)
+      end
+      render_response(data: { id: content.id })
     end
 
     def unlike
       authenticate_user
+
+      content_like = ContentLike.find_by(content_id: content.id, user_id: @user.id)
+      raise "user have not like this content" if content_like.blank?
+
+      content.transaction do
+        content.update!(count_like: content.count_like - 1)
+        content_like.delete
+      end
+      render_response(data: { id: content.id })
+    end
+
+    private
+
+    def content
+      @content ||= Content.find(content_id)
+    end
+
+    def content_id
+      params[:id]
     end
   end
 end
