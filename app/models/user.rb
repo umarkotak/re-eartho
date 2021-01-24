fb0ae5aca52d26dc01344c2ae95047a268475f1a
umar.ramadhana <umar.ramadhana@gmail.com>
2021-01-24 20:52:48 +0700

    enable multiple color avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,21 @@ class User < ApplicationRecord
   has_many :content_comments
 
   def generated_avatar_url
+    colors = ['blue', 'green', 'orange', 'purple', 'red']
     prefix = username[0].to_s.upcase
-    "http://47.254.247.135/eartho/default_avatar/#{prefix}.png"
+
+    if ['A', 'B', 'C', 'D', 'E'].include?(prefix)
+      idx = 0
+    elsif ['F', 'G', 'H', 'I', 'J'].include?(prefix)
+      idx = 1
+    elsif ['K', 'L', 'M', 'N', 'O'].include?(prefix)
+      idx = 2
+    elsif ['P', 'Q', 'R', 'S', 'T'].include?(prefix)
+      idx = 3
+    else
+      idx = 4
+    end
+
+    "http://47.254.247.135/eartho/default_avatar/#{colors[idx]}/#{prefix}.png"
   end
 end
