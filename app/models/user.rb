53e4c8d128d8d01020a74d88a988417f0b11cc85
umar.ramadhana <umar.ramadhana@gmail.com>
2021-01-24 17:45:24 +0700

    create generated avatar url

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
 
   has_many :content_likes
   has_many :content_comments
+
+  def generated_avatar_url
+    prefix = username[0].to_s.upcase
+    "http://47.254.247.135/eartho/default_avatar/#{prefix}.png"
+  end
 end
