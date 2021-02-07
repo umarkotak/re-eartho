98ddd382b581a58c1c4709095603870c19b1bbc3
umar.ramadhana <umar.ramadhana@gmail.com>
2021-02-07 22:07:10 +0700

    restructure content index api

--- app/controllers/global/contents_controller.rb
+++ app/controllers/global/contents_controller.rb
@@ -118,26 +118,30 @@ module Global
 
     def serialize_category(category)
       {
-        category_id: category.id,
-        category_type: category.category_type,
-        title: category.title,
-        contents: category.contents.map do |content|
+        feeds: [
           {
-            content_id: content.id,
-            content_type: content.content_type,
-            title: content.title,
-            description: content.description,
-            image_url: content.image_url,
-            count_like: content.count_like.to_i,
-            count_comment: content.count_comment.to_i,
-            tag: content.tag,
-            creator_id: content.user.id,
-            creator_name: content.user.username,
-            creator_avatar_url: content.user.generated_avatar_url,
-            created_at: content.created_at.localtime.strftime("%Y-%m-%d %H:%M"),
-            liked_by_me: content.liked_by_user(@user&.id)
+            category_id: category.id,
+            category_type: category.category_type,
+            title: category.title,
+            contents: category.contents.map do |content|
+              {
+                content_id: content.id,
+                content_type: content.content_type,
+                title: content.title,
+                description: content.description,
+                image_url: content.image_url,
+                count_like: content.count_like.to_i,
+                count_comment: content.count_comment.to_i,
+                tag: content.tag,
+                creator_id: content.user.id,
+                creator_name: content.user.username,
+                creator_avatar_url: content.user.generated_avatar_url,
+                created_at: content.created_at.localtime.strftime("%Y-%m-%d %H:%M"),
+                liked_by_me: content.liked_by_user(@user&.id)
+              }
+            end
           }
-        end
+        ]
       }
     end
   end
